04082ec96eba9b42118d8eb380acef05dc460b5a
BikoKiessling <biko.kiessling@gmail.com>
2020-06-21 18:12:53 +0200

    change scripts to form scripts

--- GP2020_Teilnehmer_Sheet/answer.model.ts
+++
@@ -1,8 +0,0 @@
-type dayOfArrival = "friday" | "saturday" | "sunday" | "monday";
-class answer {
-    name: string;
-    nickname: string;
-    nightCount: number;
-    dayOfArrival: dayOfArrival;
-    relativeAlcoholconsumption: number;
-}
